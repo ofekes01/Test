bbfc202e2b538300e1930362a56b1131e00bd8d6
ofer kessler <ofer.kessler@arm.com>
2019-07-11 15:48:43 +0300

    22.4

--- Apply_helm_chart.sh
+++ Apply_helm_chart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 id
 
-wget -q https://storage.googleapis.com/kubernetes-helm/helm-v2.14.1-linux-amd64.tar.gz -O - | tar -xzO linux-amd64/helm > /usr/local/sbin/helm \
+wget -q https://storage.googleapis.com/kubernetes-helm/helm-v2.14.1-linux-amd64.tar.gz -O - | /bin/tar -xzO linux-amd64/helm > /usr/local/sbin/helm \
 chmod +x /usr/local/sbin/helm
 
  wget -q https://storage.googleapis.com/kubernetes-release/release/v1.15.0/bin/linux/amd64/kubectl /usr/local/sbin/kubectl -O /usr/local/sbin/kubectl
