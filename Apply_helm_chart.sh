7f3807378a59de04e41bed9301fc37753fcf0559
ofer <ofer.kessler@arm.com>
2019-07-11 15:34:29 +0300

    22.1

--- Apply_helm_chart.sh
+++ Apply_helm_chart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+id
+
 wget -q https://storage.googleapis.com/kubernetes-helm/helm-v2.14.1-linux-amd64.tar.gz -O - | tar -xzO linux-amd64/helm > /usr/local/sbin/helm \
 chmod +x /usr/local/sbin/helm
 
