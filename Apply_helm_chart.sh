18e1e44c765b5a6fce5fe7fb29a878e5652ffa54
ofer kessler <ofer.kessler@arm.com>
2019-07-15 17:28:14 +0300

    4

--- Apply_helm_chart.sh
+++ Apply_helm_chart.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 id
 
-cd /godata/pipelines/hello_world/f5-backup
-cd /tmp
+#cd /godata/pipelines/hello_world/f5-backup
+#cd /tmp
 
-cp -rp /godata/pipelines/hello_world/f5-backup/Apply_helm_chart.sh .
+#cp -rp /godata/pipelines/hello_world/f5-backup/Apply_helm_chart.sh .
 
-wget -q https://storage.googleapis.com/kubernetes-helm/helm-v2.14.1-linux-amd64.tar.gz -O - | /bin/tar -xzO linux-amd64/helm > helm 
-chmod +x helm
+#wget -q https://storage.googleapis.com/kubernetes-helm/helm-v2.14.1-linux-amd64.tar.gz -O - | /bin/tar -xzO linux-amd64/helm > helm 
+#chmod +x helm
 
-wget -q https://storage.googleapis.com/kubernetes-release/release/v1.15.0/bin/linux/amd64/kubectl -O kubectl
-chmod +x kubectl
+#wget -q https://storage.googleapis.com/kubernetes-release/release/v1.15.0/bin/linux/amd64/kubectl -O kubectl
+#chmod +x kubectl
  
-/tmp/kubectl
-/tmp/helm
+kubectl
+helm
  
 sleep 10000
