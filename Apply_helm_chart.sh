7eaf81d969ae9b6f55b392fb31d1888f549e73c8
Ofer Kessler <ofer.kessler@arm.com>
2019-07-17 12:25:00 +0300

    ofer2

--- Apply_helm_chart.sh
+++ Apply_helm_chart.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 id
 
-#cd /godata/pipelines/hello_world/f5-backup
-#cd /tmp
+cd /home/kessler/paas-labs/ofer-cd
 
-#cp -rp /godata/pipelines/hello_world/f5-backup/Apply_helm_chart.sh .
+export KUBECONFIG=$PWD/kubeconfig
 
-#wget -q https://storage.googleapis.com/kubernetes-helm/helm-v2.14.1-linux-amd64.tar.gz -O - | /bin/tar -xzO linux-amd64/helm > helm 
-#chmod +x helm
+kubectl cluster-info
 
-#wget -q https://storage.googleapis.com/kubernetes-release/release/v1.15.0/bin/linux/amd64/kubectl -O kubectl
-#chmod +x kubectl
- 
-kubectl
-helm
- 
-sleep 10000
+splatt kubectl -n gocd get all
+
+helm list
